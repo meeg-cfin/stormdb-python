dfbf885dd4f03c710ca75f4745d0197903fb907b
Chris Bailey <bailey.cj@gmail.com>
2017-01-03 11:25:13 +0100

    tag version 0.5 (#31)

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from setuptools import setup, Command
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -8,25 +9,39 @@ from setuptools import setup, Command
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+# get the version (don't import mne here, so dependencies are not needed)
+version = None
+with open(os.path.join('stormdb', '__init__.py'), 'r') as fid:
+    for line in (line.strip() for line in fid):
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip('\'')
+            break
+if version is None:
+    raise RuntimeError('Could not determine version')
+
+
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root."""
     user_options = []
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
         os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
 
 setup(
-    name = "stormdb",
-    version = "0.5",
-    author = "Christopher Bailey",
-    author_email = "cjb@cfin.au.dk",
-    description = ("Tools for accessing StormDb @ CFIN"),
-    license = "BSD",
-    keywords = "code",
-    url = "https://github.com/meeg-cfin/stormdb-python.git",
+    name="stormdb",
+    version=version,
+    author="Christopher Bailey",
+    author_email="cjb@cfin.au.dk",
+    description=("Tools for accessing StormDb @ CFIN"),
+    license="BSD",
+    keywords="code",
+    url="https://github.com/meeg-cfin/stormdb-python.git",
     packages=['stormdb'],
     scripts=['bin/submit_to_cluster'],
     long_description=read('README.md'),
